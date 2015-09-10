b2a56274ed041ab9bd5d986d148d83854239e849
Aleksey Sheyko <aleksey_sheyko@hotmail.com>
2015-09-10 18:41:37 +0300

    Enable pausing and resuming for timer

--- app/src/main/java/com/sugarspicedance/sugarspice/TimerActivity.java
+++ app/src/main/java/com/sugarspicedance/sugarspice/TimerActivity.java
@@ -1,25 +1,38 @@
 package com.sugarspicedance.sugarspice;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class TimerActivity extends AppCompatActivity {
+
+    CountDownTimer mTimer;
+    private long mSecondsLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        int minutes = getIntent().getIntExtra("minutes", -1);
+
+        mSecondsLeft = (long) (minutes * 60);
+        TextView counterView = (TextView) findViewById(R.id.counterView);
+        counterView.setText(mSecondsLeft + "");
     }
 
     public void selectAnotherTime(View view) {
         onBackPressed();
     }
 
-    public void openTimer(View view) {
-        findViewById(R.id.logoImageView).setAlpha(0.1f);
+    public void startTimer(View view) {
+        startCountdown(mSecondsLeft);
+
+        findViewById(R.id.logoImageView).setAlpha(0.2f);
 
         Button startButton = (Button) findViewById(R.id.startButton);
         startButton.setText("P a u s e");
@@ -39,6 +52,8 @@ public class TimerActivity extends AppCompatActivity {
     }
 
     private void pauseTimer() {
+        mTimer.cancel();
+
         findViewById(R.id.logoImageView).setAlpha(0.7f);
 
         Button startButton = (Button) findViewById(R.id.startButton);
@@ -51,8 +66,35 @@ public class TimerActivity extends AppCompatActivity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openTimer(view);
+                startTimer(view);
             }
         });
+    }
+
+    private void startCountdown(long seconds) {
+        final TextView counterView = (TextView) findViewById(R.id.counterView);
+
+        mTimer = new CountDownTimer(seconds * 1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                long secondsLeft = millisUntilFinished / 1000;
+
+                counterView.setText(secondsLeft + "");
+                mSecondsLeft = secondsLeft;
+            }
+
+            public void onFinish() {
+                counterView.setText("done!");
+            }
+        };
+        mTimer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mTimer != null) {
+            mTimer.cancel();
+        }
     }
 }
