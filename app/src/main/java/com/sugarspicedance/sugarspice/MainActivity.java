8863b7e2f74c65bc0767c49b3588009ba087f86d
Aleksey Sheyko <aleksey_sheyko@hotmail.com>
2015-09-14 00:32:20 +0300

    Show booth number on the main screen

--- app/src/main/java/com/sugarspicedance/sugarspice/MainActivity.java
+++ app/src/main/java/com/sugarspicedance/sugarspice/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.parse.DeleteCallback;
 import com.parse.ParseException;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((TextView) findViewById(R.id.booth_name)).setText(ParseUser.getCurrentUser().getUsername());
     }
 
     @SuppressWarnings("unused")
